dc3b293781d7fff35eccdadac5c69dffe083b47d
omar ibrahim <omaribrahim21@rocketmail.com>
2023-06-24 19:11:27 -0400

    update on array

--- chess.js
+++ chess.js
@@ -6,7 +6,7 @@ function node(x,y){
     this.array = null;
 }
 
-class octoTree {
+class arrayTree {
     constructor(){
         this.head = null;
     }
@@ -14,27 +14,40 @@ class octoTree {
     add(x,y){
         var array1 = [];
         if (this.head == null){
-            array1.push(node(x,y));
+            newNode = new node(x,y);
+            array1.push(newNode);
             this.head = array1;
-        }
-        else if (traversal(this.head, node(x,y))){ //need to find if any ancestors have same data, if return true then we dont add this data
             return;
         }
-        else {
+        else if (preOrder(this.head, newNode)){ //need to find if any ancestors have same data, if return true then we dont add this data
+            return;
+        }
+        else {//else we add the data
             
         }
-
     }
 }
 
+//right here will be code that keeps adding different moves based on the initial position untill preOrder returns true
+
 function preOrder(head, newNode){
-    if (node != null){
+    if (head == null){ //check if head node is null and contains data , if not proceed, if yes return true
         if ((head.x == newNode.x) && (head.y == newNode.y)){
             return true;
         }
+        return false;
     }
-    else {
-        preOrder(head.one, newNode);
+    else { //if head node doesnt heave 
+        if ((head.x == newNode.x) && (head.y == newNode.y)){
+            return true;
+        }
+        arrayLength = head.length;
+        for (var i = 0; i < arrayLength; i++){
+            preOrder(head.array[i], newNode);
+            if ((head.array[i].x = newNode.x) && (head.array[i].y = newNode.y)){
+                return true;
+            }
+        }
     }
     return false;
 }
